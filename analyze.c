6d3fbec3e2267fdbfd52e83eec79119ccf1c7be4
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-02-06 16:58:34 -0300

    0 mudancas nadandanadnsad

--- analyze.c
+++ analyze.c
@@ -41,6 +41,15 @@ static void nullProc(TreeNode *t) {
     return;
 }
 
+/* Função symbolError imprime um erro durante a tabela de simbolos
+ * tal erro tambem é um erro semantico
+ */
+static void symbolError(TreeNode *t, char *message) {
+  fprintf(listing, "ERRO SEMANTICO: %s LINHA: %d\n", message, t->lineno);
+  Error = TRUE;
+  exit(-1);
+}
+
 /* Procedure insertNode inserts
  * identifiers stored in t into
  * the symbol table
@@ -48,36 +57,104 @@ static void nullProc(TreeNode *t) {
 static void insertNode(TreeNode *t) {
   switch (t->nodekind) {
   case StmtK:
-    switch (t->kind.stmt) {
-    case AssignK:
-    case ReadK:
-      if (st_lookup(t->attr.name) == -1)
-        /* not yet in table, so treat as new definition */
-        st_insert(t->attr.name, t->lineno, location++);
-      else
-        /* already in table, so ignore location,
-           add line number of use only */
-        st_insert(t->attr.name, t->lineno, 0);
-      break;
-    default:
-      break;
-    }
     break;
+
   case ExpK:
     switch (t->kind.exp) {
     case IdK:
-      if (st_lookup(t->attr.name) == -1)
+    case ArrIdK:
+    case CallK: {
+      if (st_lookup(t->attr.name) == -1) {
         /* not yet in table, so treat as new definition */
         st_insert(t->attr.name, t->lineno, location++);
-      else
+      } else
         /* already in table, so ignore location,
            add line number of use only */
         st_insert(t->attr.name, t->lineno, 0);
       break;
+    }
     default:
       break;
     }
     break;
+
+  case DeclK:
+    switch (t->kind.decl) {
+    case FunK:
+      /* Look up scope list to check scope existence */
+      if (st_lookup(t->attr.name) != -1) {
+        symbolError(t, "Redefinição de função");
+        break;
+      }
+
+      if (st_lookup(t->attr.name) == -1) {
+        /* not yet in table, so treat as new definition */
+        st_insert(t->attr.name, t->lineno, location++);
+      }
+      break;
+    case VarK:
+      /* Look up to check variable existence */
+      if (st_lookup(t->attr.name) != -1) {
+        symbolError(t, "Redefinição de variável");
+        break;
+      }
+      // Type Checing : Type should not be void
+      if (t->child[0]->type == Void) {
+        symbolError(t, "Variável não pode ser do tipo void");
+        break;
+      }
+
+      st_insert(t->attr.name, t->lineno, location++);
+      break;
+    case ArrVarK:
+      // Type Checing : Type should not be void
+      if (t->child[0]->type == Void) {
+        symbolError(t, "Variável do tipo array não pode ser void");
+        break;
+      }
+
+      /*  Look up to check array variable existence  */
+      if (st_lookup(t->attr.arr.name) != -1) {
+        symbolError(t, "Variável do tipo array já declarada");
+        break;
+      }
+
+      st_insert(t->attr.name, t->lineno, location++);
+      break;
+    case ArrParamK:
+      if (t->attr.name == NULL) {
+        break;
+      }
+
+      // Type Checing : Type should not be void
+      if (t->child[0]->type == Void) {
+        symbolError(t, "Parâmetro do tipo array não pode ser void");
+        break;
+      }
+
+      /*  Look up to check array variable existence  */
+      if (st_lookup(t->attr.name) != -1) {
+        symbolError(t, "Parâmetro do tipo array já declarado");
+        break;
+      }
+
+      st_insert(t->attr.name, t->lineno, location++);
+      break;
+    case ParamK:
+      if (t->attr.name != NULL) {
+        /*  Look up to check parameter existence  */
+        if (st_lookup(t->attr.name) != -1) {
+          symbolError(t, "Redefinição de parâmetro");
+          break;
+        }
+
+        st_insert(t->attr.name, t->lineno, location++);
+        break;
+      }
+      break;
+    default:
+      break;
+    }
   default:
     break;
   }
@@ -89,7 +166,7 @@ static void insertNode(TreeNode *t) {
 void buildSymtab(TreeNode *syntaxTree) {
   traverse(syntaxTree, insertNode, nullProc);
   if (TraceAnalyze) {
-    fprintf(listing, "\nSymbol table:\n\n");
+    fprintf(listing, "\nTabela de símbolos:\n\n");
     printSymTab(listing);
   }
 }
@@ -104,42 +181,30 @@ static void typeError(TreeNode *t, char *message) {
  */
 static void checkNode(TreeNode *t) {
   switch (t->nodekind) {
-  case ExpK:
-    switch (t->kind.exp) {
-    case OpK:
-      if ((t->child[0]->type != Integer) || (t->child[1]->type != Integer))
-        typeError(t, "Op applied to non-integer");
-      if ((t->attr.op == EQ) || (t->attr.op == LT))
-        t->type = Boolean;
-      else
-        t->type = Integer;
-      break;
-    case ConstK:
-    case IdK:
-      t->type = Integer;
-      break;
-    default:
-      break;
-    }
-    break;
   case StmtK:
     switch (t->kind.stmt) {
-    case IfK:
-      if (t->child[0]->type == Integer)
-        typeError(t->child[0], "if test is not Boolean");
+    case AssignK: {
+      // Verify the type match of two operands when assigning.
+      if (t->child[0]->attr.arr.type == IntegerArray) {
+        typeError(t->child[0], "Assignment to Integer Array Variable");
+      }
+
+      if (t->child[0]->attr.type == Void) {
+        typeError(t->child[0], "Assignment to Void Variable");
+      }
       break;
-    case AssignK:
-      if (t->child[0]->type != Integer)
-        typeError(t->child[0], "assignment of non-integer value");
-      break;
-    case WriteK:
-      if (t->child[0]->type != Integer)
-        typeError(t->child[0], "write of non-integer value");
-      break;
-    case RepeatK:
-      if (t->child[1]->type == Integer)
-        typeError(t->child[1], "repeat test is not Boolean");
-      break;
+    }
+    case ReturnK: {
+      const TreeNode *funcDecl;
+      const ExpType funcType = funcDecl->type;
+      const TreeNode *expr = t->child[0];
+
+      if (funcType == Void && (expr != NULL && expr->type != Void)) {
+        typeError(t, "expected no return value");
+      } else if (funcType == Integer && (expr == NULL || expr->type == Void)) {
+        typeError(t, "expected return value");
+      }
+    }
     default:
       break;
     }
