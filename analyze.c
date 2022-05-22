f83bc5ec149d717dbc834aa68c94ee2e53358b9e
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-05-22 00:07:07 -0300

    Revert "Limite da linha aumentado de 80 para 100"
    This reverts commit d0140815c855fd6edb66ea5864d0cf53b204ec05.
    Limite de 80 vai ser usado por enquanto, sendo o limite de 100 linhas
    ponderado no futuro.

--- analyze.c
+++ analyze.c
@@ -36,7 +36,8 @@ static void verificaMain(TreeNode *t) {
   int linhaMain = st_lookup("main");
   if (linhaMax > linhaMain || varLinhaMax > linhaMain) {
     int max = linhaMax > varLinhaMax ? linhaMax : varLinhaMax;
-    fprintf(listing, "ERRO SEMANTICO: declaração depois do main LINHA: %d\n", max);
+    fprintf(listing, "ERRO SEMANTICO: declaração depois do main LINHA: %d\n",
+            max);
     Error = TRUE;
     exit(-1);
   }
@@ -127,7 +128,8 @@ static void insertOutput(void) {
  * it applies preProc in preorder and postProc
  * in postorder to tree pointed to by t
  */
-static void traverse(TreeNode *t, void (*preProc)(TreeNode *), void (*postProc)(TreeNode *)) {
+static void traverse(TreeNode *t, void (*preProc)(TreeNode *),
+                     void (*postProc)(TreeNode *)) {
   if (t != NULL) {
     preProc(t);
     for (int i = 0; i < MAXCHILDREN; i++) {
@@ -253,7 +255,8 @@ static void insertNode(TreeNode *t) {
         break;
       }
 
-      st_insert(t->attr.arr.name, varFun, tipo, t->scope, t->lineno, location++);
+      st_insert(t->attr.arr.name, varFun, tipo, t->scope, t->lineno,
+                location++);
       break;
     case ArrParamK:
       if (t->attr.name == NULL) {
@@ -380,4 +383,6 @@ static void checkNode(TreeNode *t) {
 /* Procedure typeCheck performs type checking
  * by a postorder syntax tree traversal
  */
-void typeCheck(TreeNode *syntaxTree) { traverse(syntaxTree, nullProc, checkNode); }
+void typeCheck(TreeNode *syntaxTree) {
+  traverse(syntaxTree, nullProc, checkNode);
+}
