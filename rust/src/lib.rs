07753a4d7bdc250da1b53a98aff958ca26e43929
Miguel Taciano <40744030+mtaciano@users.noreply.github.com>
2022-07-04 22:02:48 -0300

    Teste da ffi

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,16 +4,29 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[no_mangle]
-pub extern "C" fn this_is_a_test() -> i32 {
+pub extern "C" fn make_assembly(quad: Quad) -> i32 {
+    let mut curr_quad = quad;
+    while let Some(q) = unsafe { curr_quad.as_ref() } {
+        let cmd = unsafe { std::ffi::CStr::from_ptr(q.command) };
+        let arg1 = unsafe { std::ffi::CStr::from_ptr(q.arg1) };
+        let arg2 = unsafe { std::ffi::CStr::from_ptr(q.arg2) };
+        let arg3 = unsafe { std::ffi::CStr::from_ptr(q.arg3) };
+        println!("{:?}, {:?}, {:?}, {:?}", cmd, arg1, arg2, arg3);
+        unsafe {
+            curr_quad = curr_quad.as_ref().unwrap().next;
+        };
+    }
     0
 }
 
-#[cfg(test)]
+/* #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        assert_eq!(this_is_a_test(), 0);
+        unsafe {
+            assert_eq!(make_assembly(magic), 0);
+        }
     }
-}
+} */
